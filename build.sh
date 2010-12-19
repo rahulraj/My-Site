616b332c8bebafbc1464f156f2729c7005826699
Rahul Rajagopalan <rahulrajago@gmail.com>
2010-12-19 14:19:22 -0500

    Modified the build script to also compress CSS via the YUI compressor

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+# The YUI compressor compresses CSS (jsmin doesn't) but when I tried to
+# use it to compress my JavaScript, it broke the functionality. So I use
+# jsmin for JavaScript and YUI for CSS
 jsmin <colors.js> minified/colors.js "Copyright (c) 2010 Rahul Rajagopalan"
 jsmin <highlightNav.js> minified/highlightNav.js "Copyright (c) 2010 Rahul Rajagopalan"
+yuiCompress.py -o minified/firstStyle.css firstStyle.css
