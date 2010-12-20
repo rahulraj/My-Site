289c2d6dd16552bf2fe81652657128f86a7468a5
Rahul Rajagopalan <rahulrajago@gmail.com>
2010-12-19 22:46:50 -0500

    Used slideToggle to make the code more concise

--- showHide.js
+++ showHide.js
@@ -5,26 +5,17 @@ $(function() {
     // JavaScript enabled; this is to not confuse them by showing them
     // a nonfunctional button. Make it appear now.
     toggleVisibility.css("display", "inline");
-   
-    var hideArticle = function(contentToHide) {
-        contentToHide.slideUp();
-    };
-
-    var showArticle = function(contentToShow) {
-        contentToShow.slideDown();
-    }
 
     toggleVisibility.click(function() {
         // find the content whose visibility will be toggled
         var parentArticle = $(this).parents("article");
         var contentToChange = parentArticle.children().not("h3");
         // hide if shown, show if hidden
+        contentToChange.slideToggle();
         if ($(this).val() === "Hide") {
-            hideArticle(contentToChange); 
             $(this).val("Show");
         }
         else {
-            showArticle(contentToChange);
             $(this).val("Hide");
         }
     });
