fab7fe2b8f3d9eb5b8a3bca02e1369b6b1d08442
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-01-05 12:18:44 -0700

    Pulled the changes to replce the show/hide buttons with anchors

--- showHide.js
+++ showHide.js
@@ -1,26 +1,4 @@
-$(function() {
-    var toggleVisibility = $("article").find("input");
+// Copyright (c) 2010 Rahul Rajagopalan
 
-    // toggleVisibility is hidden by default for users who don't have
-    // JavaScript enabled; this is to not confuse them by showing them
-    // a nonfunctional button. Make it appear now.
-    toggleVisibility.css("display", "inline");
-
-    toggleVisibility.click(function() {
-        // find the content whose visibility will be toggled
-        var parentArticle = $(this).parents("article");
-        // leave the title in the h3 tags showing so the user will know
-        // what will appear when they click Show
-        var contentToChange = parentArticle.children().not("h3");
-        // hide if shown, show if hidden
-        // jQuery's slideToggle method does the heavy lifting
-        contentToChange.slideToggle();
-        // Change the button's value to be the function it will perform
-        if ($(this).val() === "Hide") {
-            $(this).val("Show");
-        }
-        else {
-            $(this).val("Hide");
-        }
-    });
-});
+$(function(){var toggleVisibility=$("h3 > a");toggleVisibility.css("display","inline");toggleVisibility.click(function(){var parentArticle=$(this).parents("article");var contentToChange=parentArticle.children().not("h3");contentToChange.slideToggle();if($(this).html()==="Hide"){$(this).html("Show");}
+else{$(this).html("Hide");}});});
