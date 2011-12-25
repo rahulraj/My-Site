750419d256fb65aefba22f362442a3ec62df3877
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-12-25 13:32:14 -0500

    Got AJAX loading to work (still need to fix the back button and the scripts).

--- highlightNav.js
+++ highlightNav.js
@@ -1,43 +1,62 @@
+var site = {};
+
+site.navItemsWithAnchors = function() {
+  return $('nav').find('li').filter(':has(a)');
+};
+
+site.removeEvents = function() {
+  var navItems = $('nav li');
+  navItems.trigger('mouseleave');
+  navItems.unbind('click');
+  navItems.unbind('mouseenter');
+  navItems.unbind('mouseleave');
+  navItems.css('cursor', 'default');
+};
+
+site.addHoverEventsToAnchors = function() {
+  /*
+   * This script uses jQuery to add a highlighting visual
+   * effect to the navigation sidebar. When the user's mouse hovers
+   * over one of the links in the sidebar, the li that the
+   * link is part has its background-color highlighted with
+   * highlightCol.
+   */
+  var navItemsWithAnchors = site.navItemsWithAnchors();
+  var highlightCol = "#FF6600"; // orange
+  // Make them be highlighted on mouseover
+  navItemsWithAnchors.bind('mouseenter', function() {
+        // Change to highlightCol, using animate for a
+        // fade-in effect, with help from the color plugin
+        // to jQuery
+        var thisItem = $(this); // cache $(this)
+        thisItem.stop(); // prevent repeated fadeins/outs from queueing
+        thisItem.animate({backgroundColor: highlightCol}, 'fast');
+  });
+  navItemsWithAnchors.bind('mouseleave', function() {
+        // Get the value of the old background color 
+        var sidebarCol = $("nav").css("background-color");
+        // NOTE: I had to get the color in this function and
+        // not elsewhere since the color might have changed
+        // since the page loaded if the user changes it
+        // using colors.js
+        // Revert to it 
+        var thisItem = $(this);
+        thisItem.stop();
+        thisItem.animate({backgroundColor: sidebarCol}, 'fast');
+  });
+
+  /*
+  // Allow the user to click the li's to follow the link
+  navItemsWithAnchors.bind('click', function() {
+    var myAnchor = $(this).children('a');
+    myAnchor.trigger('click');
+  });
+  */
+  // Since the li's are clickable, change the cursor to the
+  // pointer to reflect this
+  navItemsWithAnchors.css("cursor", "pointer");
+};
+
 $(function() {
-    /*
-     * This script uses jQuery to add a highlighting visual
-     * effect to the navigation sidebar. When the user's mouse hovers
-     * over one of the links in the sidebar, the li that the
-     * link is part has its background-color highlighted with
-     * highlightCol.
-     */
-    var navItemsWithAnchors = $("nav").find("li").filter(":has(a)");
-    var highlightCol = "#FF6600"; // orange
-    // Make them be highlighted on mouseover
-    navItemsWithAnchors.hover(
-        function() { /*On mouseover*/
-            // Change to highlightCol, using animate for a
-            // fade-in effect, with help from the color plugin
-            // to jQuery
-            var thisItem = $(this); // cache $(this)
-            thisItem.stop(); // prevent repeated fadeins/outs from queueing
-            thisItem.animate({backgroundColor: highlightCol}, 'fast');
-        },
-        function() { /*On mouseout*/
-            // Get the value of the old background color 
-            var sidebarCol = $("nav").css("background-color");
-            // NOTE: I had to get the color in this function and
-            // not elsewhere since the color might have changed
-            // since the page loaded if the user changes it
-            // using colors.js
-            // Revert to it 
-            var thisItem = $(this);
-            thisItem.stop();
-            thisItem.animate({backgroundColor: sidebarCol}, 'fast');
-        }
-    );
-   
-    // Allow the user to click the li's to follow the link
-    navItemsWithAnchors.click(function() {
-        var href = $(this).children("a").attr("href");
-        window.location.replace(href);
-    });
-    // Since the li's are clickable, change the cursor to the
-    // pointer to reflect this
-    navItemsWithAnchors.css("cursor", "pointer");
+  site.addHoverEventsToAnchors();
 });
--- src/ajaxify/main.cljs
+++ src/ajaxify/main.cljs
@@ -2,18 +2,20 @@
 
 (def main-text-id "#mainText")
 
-(defn ^:export navigational-anchors []
-  (js/jQuery "nav a"))
-
-(defn html [jquery-object]
+(defn html
+  "Helper function to get the HTML inside a JQuery object.
+   Needed because ClojureScript syntax for no-argument
+   object methods is a bit awkward."
+  [jquery-object]
   (. jquery-object (html)))
 
-(defn main-text-from-load-container [container]
+(defn main-text-from-load-container
+  [container]
   (-> container
     (.find main-text-id)
     (html)))
 
-(defn ^:export on-anchor-click [anchor]
+(defn load-content [anchor]
   (let [anchors-href (.attr anchor "href")
         to-load (str anchors-href " " main-text-id)
         load-container (js/jQuery "<span>")
@@ -21,13 +23,49 @@
     (.fadeOut main-text-element "fast" (fn []
       (.load load-container to-load (fn []
         (let [new-main-text (main-text-from-load-container load-container)]
-          (console/log (str "new-main-text " new-main-text))
           (.html main-text-element new-main-text)
           (.fadeIn main-text-element "fast"))))))))
+
+(defn current-page-relative-href []
+  (let [location (.location js/window)
+        url location/href
+        parts (.split url "/")]
+    (last parts)))
+
+(defn parent [jquery-object]
+  (. jquery-object (parent)))
+
+(defn restyle-anchors [clicked-anchor]
+  (let [current-page-href (current-page-relative-href)
+        current-page-anchor (js/jQuery (str "<a href=" current-page-href ">"))
+        current-page-list-item (js/jQuery "#currentPage")
+        current-page-html (html current-page-list-item)
+        clicked-list-item (parent clicked-anchor)]
+    (.html current-page-anchor current-page-html)
+    (-> current-page-list-item
+      (.removeAttr "id")
+      (.html current-page-anchor))
+    (-> clicked-list-item
+      (.attr "id" "currentPage")
+      (.html (html clicked-anchor))
+    (site/removeEvents)
+    (site/addHoverEventsToAnchors))))
+
+(defn update-history [clicked-anchor]
+  (let [new-href (.attr clicked-anchor "href")]
+    (.pushState js/history {} "State" new-href)))
+
+(defn ^:export on-anchor-click [anchor]
+  ; TODO Use the History API to make back/forward work again.
+  (load-content anchor)
+  (restyle-anchors anchor)
+  (update-history anchor))
 
 (defn ^:export main
   "Entry point for the Clojurescript, to be called on load"
   [& args]
-  (console/log "this is " this)
-  (.append (js/jQuery "body") "<div>hello</div>")
-  "Hi there")
+  ;(console/log "this is " js/this) ;nope, doesn't work
+  (let [my-object {:name "rahul"}] ; TODO figure this out
+    (console/log (str "my name is " (.name my-object)))
+    (.append (js/jQuery "body") "<div>hello</div>")
+    my-object))
