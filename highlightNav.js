1e36ceaf2284634fd75f5bb439a059022903d06a
Rahul Rajagopalan <rahulrajago@gmail.com>
2012-01-13 01:11:51 -0500

    Refactored highlightNav

--- highlightNav.js
+++ highlightNav.js
@@ -5,12 +5,12 @@ site.navItemsWithAnchors = function() {
 };
 
 site.removeEvents = function() {
-  var navItems = $('nav li');
-  navItems.trigger('mouseleave');
-  navItems.off('click');
-  navItems.off('mouseenter');
-  navItems.off('mouseleave');
-  navItems.css('cursor', 'default');
+  $('nav li').
+      trigger('mouseleave').
+      off('click').
+      off('mouseenter').
+      off('mouseleave').
+      css('cursor', 'default');
 };
 
 site.addHoverEventsToAnchors = function() {
@@ -19,30 +19,29 @@ site.addHoverEventsToAnchors = function() {
    * effect to the navigation sidebar. When the user's mouse hovers
    * over one of the links in the sidebar, the li that the
    * link is part has its background-color highlighted with
-   * highlightCol.
+   * highlightColor.
    */
   var navItemsWithAnchors = site.navItemsWithAnchors();
-  var highlightCol = "#FF6600"; // orange
+  var highlightColor = "#FF6600"; // orange
   // Make them be highlighted on mouseover
   navItemsWithAnchors.on('mouseenter', function() {
-        // Change to highlightCol, using animate for a
+        // Change to highlightColor, using animate for a
         // fade-in effect, with help from the color plugin
         // to jQuery
-        var thisItem = $(this); // cache $(this)
-        thisItem.stop(); // prevent repeated fadeins/outs from queueing
-        thisItem.animate({backgroundColor: highlightCol}, 'fast');
+        $(this).
+            stop(). // prevent repeated fadeins/outs from queueing
+            animate({backgroundColor: highlightColor}, 'fast');
   });
   navItemsWithAnchors.on('mouseleave', function() {
         // Get the value of the old background color 
-        var sidebarCol = $("nav").css("background-color");
+        var sidebarColor = $("nav").css("background-color");
         // NOTE: I had to get the color in this function and
         // not elsewhere since the color might have changed
-        // since the page loaded if the user changes it
-        // using colors.js
+        // via colors.js
         // Revert to it 
-        var thisItem = $(this);
-        thisItem.stop();
-        thisItem.animate({backgroundColor: sidebarCol}, 'fast');
+        $(this).
+            stop().
+            animate({backgroundColor: sidebarColor}, 'fast');
   });
 
   // Since the li's are clickable, change the cursor to the
