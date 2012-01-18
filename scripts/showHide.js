49ea339eafd2cb41e7c3bb961340e0d5b111c511
Rahul Rajagopalan <rahulrajago@gmail.com>
2012-01-18 13:46:06 -0500

    Updated showHide.js to work with the new layout.

--- scripts/showHide.js
+++ scripts/showHide.js
@@ -1,26 +1,26 @@
 $(function() {
-  var toggleVisibility = $('#mainText h3 a');
+  var toggleVisibility = $('section').find('h3').find('a');
 
   // toggleVisibility is hidden by default for users who don't have
   // JavaScript enabled; this is to not confuse them by showing them
   // a nonfunctional link. Make it appear now.
   toggleVisibility.css("display", "inline");
 
-  $('#mainText').on('click', 'h3 a', function(event) {
+  toggleVisibility.on('click', function(event) {
     event.preventDefault();
     event.stopPropagation();
     // find the content whose visibility will be toggled
-    var thisItem = $(this);
-    var parentArticle = thisItem.parents("article");
+    var thisAnchor = $(this);
+    var parentSection = thisAnchor.parents('section');
     // leave the title in the h3 tags showing so the user will know
     // what will appear when they click Show
-    var contentToChange = parentArticle.children().not("h3");
+    var contentToChange = parentSection.children().not("h3");
     // hide if shown, show if hidden
     // jQuery's slideToggle method does the heavy lifting
     contentToChange.slideToggle();
     // Change the anchor's inner HTML value to be the function it will perform
-    var newHtml = thisItem.html() === "Hide" ? "Show" : "Hide";
-    thisItem.html(newHtml);
+    var newHtml = thisAnchor.html() === "Hide" ? "Show" : "Hide";
+    thisAnchor.html(newHtml);
     return false;
   });
 });
